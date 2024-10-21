231e72a1c71293b5a849487dd8b83ee1a7cda196
Onne164 <summaarum@gmail.com>
2024-10-21 11:23:22 -0400

    Fix template rendering error in exam results page

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -4,21 +4,25 @@ from django.conf.urls.static import static
 from . import views
 
 app_name = 'onlinecourse'
+
 urlpatterns = [
-    # route is a string contains a URL pattern
-    # view refers to the view function
-    # name the URL
-    path(route='', view=views.CourseListView.as_view(), name='index'),
+    # Route for the course list view
+    path('', views.CourseListView.as_view(), name='index'),
+    
+    # Routes for user registration and authentication
     path('registration/', views.registration_request, name='registration'),
     path('login/', views.login_request, name='login'),
     path('logout/', views.logout_request, name='logout'),
-    # ex: /onlinecourse/5/
+    
+    # Route for course details
     path('<int:pk>/', views.CourseDetailView.as_view(), name='course_details'),
-    # ex: /enroll/5/
+    
+    # Route for enrolling in a course
     path('<int:course_id>/enroll/', views.enroll, name='enroll'),
-
-    # <HINT> Create a route for submit view
-
-    # <HINT> Create a route for show_exam_result view
-
- ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+    # Route for submitting the exam
+    path('<int:course_id>/submit/', views.submit, name='submit'),
+    
+    # Route for showing exam results
+    path('course/<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name='exam_result'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
